f28da865c20812d851d704fd53960a410743ad6d
Brian Melton <brianmel@microsoft.com>
2015-09-08 13:22:30 -0700

    Adding gulpfile config

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+gulp.task('default', function() {
+    var config = {
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT: 8000
+        },
+        ignore: ['./node_modules/**']
+    };
+    nodemon(config).on('restart', function() {
+        console.log('Restarting');
+    });
+});
