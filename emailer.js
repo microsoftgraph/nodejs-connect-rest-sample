3615f0bbaef192296d90473486307e8c9eccd2b7
Brian Melton <brianmel@microsoft.com>
2015-11-17 13:48:04 -0800

    Updating src license to match rename

--- emailer.js
+++ emailer.js
@@ -52,7 +52,7 @@ exports.generatePostBody = generatePostBody;
 
 /*
 ######################################################################
-O365-Nodejs-Unified-API-Connect, https://github.com/OfficeDev/O365-Nodejs-Unified-API-Connect
+O365-Nodejs-Microsoft-Graph-Connect, https://github.com/OfficeDev/O365-Nodejs-Microsoft-Graph-Connect
 
 Copyright (c) Microsoft Corporation
 All rights reserved.
--- routes/index.js
+++ routes/index.js
@@ -126,7 +126,7 @@ module.exports = router;
 
 /*
 ######################################################################
-O365-Nodejs-Unified-API-Connect, https://github.com/OfficeDev/O365-Nodejs-Unified-API-Connect
+O365-Nodejs-Microsoft-Graph-Connect, https://github.com/OfficeDev/O365-Nodejs-Microsoft-Graph-Connect
 
 Copyright (c) Microsoft Corporation
 All rights reserved.
