ac8fe67770ba3402e9aaf92f210123956b3a7d89
Ricardo Loo <ricardol@microsoft.com>
2016-04-13 11:10:58 -0700

    Adding eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+  },
+  'extends': 'airbnb/legacy',
+  'rules':{
+    'prefer-template': 0,
+    'comma-dangle': 0,
+    'func-names': 0,
+    'prefer-arrow-callback': 0,
+    'new-cap': 0,
+    'no-param-reassign': 0,
+    'no-use-before-define': 0,
+    'no-console': 0
+  }
+};
