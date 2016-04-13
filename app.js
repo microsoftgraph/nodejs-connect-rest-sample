65c620f15fed70bfb25aa61e54859e6349e23f2c
Ricardo Loo <ricardol@microsoft.com>
2016-04-13 11:13:26 -0700

    Adding favicon

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
