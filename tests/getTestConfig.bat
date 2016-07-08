a7dabcc5ce1959dbb471f1d780ca21ec067e9b34
Ricardo Loo <ricardol@microsoft.com>
2016-07-08 13:33:10 -0700

    Only receive the password as a parameter from the command-line

--- tests/getTestConfig.bat
+++ tests/getTestConfig.bat
@@ -2,9 +2,9 @@
 
 SET testConfig={
 SET testConfig=%testConfig% "test_client_id": "%TEST_CLIENT_ID%",
-SET testConfig=%testConfig% "test_client_secret": "%1",
+SET testConfig=%testConfig% "test_client_secret": "%TEST_CLIENT_SECRET%",
 SET testConfig=%testConfig% "test_username": "%TEST_USERNAME%",
-SET testConfig=%testConfig% "test_password": "%2"
+SET testConfig=%testConfig% "test_password": "%1"
 SET testConfig=%testConfig%  }
 echo %testConfig%
 echo %testConfig% > testConfig.json
--- tests/getTestConfig.sh
+++ tests/getTestConfig.sh
@@ -2,9 +2,9 @@
 
 testConfig="{
     \"test_client_id\": \"$TEST_CLIENT_ID\",
-    \"test_client_secret\": \"$1\",
+    \"test_client_secret\": \"$TEST_CLIENT_SECRET\",
     \"test_username\": \"$TEST_USERNAME\",
-    \"test_password\": \"$2\"
+    \"test_password\": \"$1\"
 }"
 echo $testConfig
 echo $testConfig > testConfig.json
