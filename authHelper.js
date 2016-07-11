37d094c48187386e5ec1e018be4dfd6816b6a7a5
Ricardo Loo <ricardol@microsoft.com>
2016-07-11 15:57:11 -0700

    Moved endpoints to properties in the credentials object
    Also used a simpler callback in operations

--- authHelper.js
+++ authHelper.js
@@ -8,9 +8,13 @@ var OAuth = require('oauth');
 // The application registration (must match Azure AD config)
 var credentials = {
   authority: 'https://login.microsoftonline.com/common',
+  authorize_endpoint: '/oauth2/authorize',
+  token_endpoint: '/oauth2/token',
+  logout_endpoint: '/oauth2/logout',
   client_id: 'ENTER_YOUR_CLIENT_ID',
   client_secret: 'ENTER_YOUR_SECRET',
-  redirect_uri: 'http://localhost:3000/login'
+  redirect_uri: 'http://localhost:3000/login',
+  resouce: 'https://graph.microsoft.com/'
 };
 
 /**
@@ -18,7 +22,7 @@ var credentials = {
  * @return {string} a fully formed uri with which authentication can be completed
  */
 function getAuthUrl() {
-  return credentials.authority + '/oauth2/authorize' +
+  return credentials.authority + credentials.authorize_endpoint +
     '?client_id=' + credentials.client_id +
     '&response_type=code' +
     '&redirect_uri=' + credentials.redirect_uri;
@@ -27,17 +31,16 @@ function getAuthUrl() {
 /**
  * Gets a token for a given resource.
  * @param {string} code An authorization code returned from a client.
- * @param {string} res A URI that identifies the resource for which the token is valid.
  * @param {AcquireTokenCallback} callback The callback function.
  */
-function getTokenFromCode(res, code, callback) {
+function getTokenFromCode(code, callback) {
   var OAuth2 = OAuth.OAuth2;
   var oauth2 = new OAuth2(
     credentials.client_id,
     credentials.client_secret,
     credentials.authority,
-    '/oauth2/authorize',
-    '/oauth2/token'
+    credentials.authorize_endpoint,
+    credentials.token_endpoint
   );
 
   oauth2.getOAuthAccessToken(
@@ -45,7 +48,7 @@ function getTokenFromCode(res, code, callback) {
     {
       grant_type: 'authorization_code',
       redirect_uri: credentials.redirect_uri,
-      resource: 'https://graph.microsoft.com/'
+      resource: credentials.resouce
     },
     function(e, access_token, refresh_token, results){
       callback(e, access_token, refresh_token);
@@ -58,33 +61,29 @@ function getTokenFromCode(res, code, callback) {
  * Gets a new access token via a previously issued refresh token.
  * @param {string} res The OAuth resource for which a token is being request.
  *                     This parameter is optional and can be set to null.
- * @param {string} token A refresh token returned in a token response
- *                       from a previous invocation of acquireToken.
+ * @param {string} refreshToken A refresh token returned in a token response
+ *                       from a previous result of an authentication flow.
  * @param {AcquireTokenCallback} callback The callback function.
  */
-function getTokenFromRefreshToken(res, token, callback) {
+function getTokenFromRefreshToken(refreshToken, callback) {
   var OAuth2 = OAuth.OAuth2;
   var oauth2 = new OAuth2(
     credentials.client_id,
     credentials.client_secret,
     credentials.authority,
-    '/oauth2/authorize',
-    '/oauth2/token'
+    credentials.authorize_endpoint,
+    credentials.token_endpoint
   );
 
   oauth2.getOAuthAccessToken(
-    token,
+    refreshToken,
     {
       grant_type: 'refresh_token',
       redirect_uri: credentials.redirect_uri,
-      resource: 'https://graph.microsoft.com/'
+      resource: credentials.resouce
     },
     function(e, access_token, refresh_token, results){
-      if (e) {
-        callback(null);
-      } else {
-        callback(results);
-      }
+      callback(e, results);
     }
   );
 }
