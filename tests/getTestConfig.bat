f1908bc095226b58e6bcd33353eb242651bbf1a6
Ricardo Loo <ricardol@microsoft.com>
2016-07-19 12:09:11 -0700

    Updated integration test with new variables
    Forcing new commit to run tests.

--- tests/getTestConfig.bat
+++ tests/getTestConfig.bat
@@ -1,6 +1,8 @@
 @echo off
 
 SET testConfig={
+SET testConfig=%testConfig% "test_client_id_v1": "%TEST_CLIENT_ID_V1%",
+SET testConfig=%testConfig% "test_client_secret_v1": "%TEST_CLIENT_SECRET_V1%",
 SET testConfig=%testConfig% "test_client_id_v2": "%TEST_CLIENT_ID_V2%",
 SET testConfig=%testConfig% "test_client_secret_v2": "%TEST_CLIENT_SECRET_V2%",
 SET testConfig=%testConfig% "test_username": "%TEST_USERNAME%",
